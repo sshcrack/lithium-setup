3e51cbc71289127c88b80d51280e9d6ec311b130
sshcrack <34072808+sshcrack@users.noreply.github.com>
2021-03-02 22:18:48 +0100

    Update remmina.sh

--- remmina.sh
+++ remmina.sh
@@ -2,4 +2,4 @@
 usr=$(users)
 rm -v /home/$usr/.local/share/keyrings/*
 cd /home/$usr
-su $usr -c 'sleep 5 && cd /home/$(users) && remmina -c profile.remmina > log.log' &
+su $usr -c 'cd /home/$(users) && remmina -c profile.remmina > log.log' &
