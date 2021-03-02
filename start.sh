02497d1892eb495488432b26daa9e2cde602dac3
sshcrack <34072808+sshcrack@users.noreply.github.com>
2021-03-02 10:49:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/sshcrack/lithium-setup/master/download.sh)"
+sh -c "$(curl -fsSL -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/sshcrack/lithium-setup/master/download.sh)"
