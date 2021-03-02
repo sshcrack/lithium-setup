054bf22ceb85bd8ba2463965d0109e34a24d025a
sshcrack <34072808+sshcrack@users.noreply.github.com>
2021-03-02 11:34:15 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ cat ./sources.list > /etc/apt/sources.list
 apt update
 apt upgrade -y
 apt dist-upgrade -y
-apt install openssh-server openssh-client git remmina remmina-plugin-rdp -y
+apt install openssh-server openssh-client git remmina remmina-plugin-rdp net-tools -y
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
 cd ~/.ssh
 cat id_rsa.pub >> authorized_keys
