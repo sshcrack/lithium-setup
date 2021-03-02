b1ad2fa4dc9725121c0dbb4fb8ac36ee5d0c591f
sshcrack <34072808+sshcrack@users.noreply.github.com>
2021-03-02 11:14:26 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,9 +6,7 @@ cat ./sources.list > /etc/apt/sources.list
 apt update
 apt upgrade -y
 apt dist-upgrade -y
-apt install openssh-server -y
-apt install openssh-client -y
-apt install git -y
+apt install openssh-server openssh-client git -y
 ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
 cd ~/.ssh
 cat id_rsa.pub >> authorized_keys
